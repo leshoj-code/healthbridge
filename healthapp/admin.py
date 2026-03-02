068112c1bc8b2bc23fed5ff25f64696b433d7f60
leshoj-code <leshyoj@gmail.com>
2026-03-02 13:57:02 +0300

    Mpesa

--- healthapp/admin.py
+++ healthapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 
-from healthapp.models import Patient
+from healthapp.models import *
 
-from healthapp.models import Myappointment
+
 
 # Register your models here.
 admin.site.register(Patient)
 
 admin.site.register(Myappointment)
+
+admin.site.register(Transaction)
