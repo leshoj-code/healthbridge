74a24e8414c7510b8cb662f2554189a05245a966
YourUsername <you@example.com>
2026-03-05 12:04:36 +0300

    hosi

--- healthapp/urls.py
+++ healthapp/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
 
 
 
-    path('register/', views.register, name='register'),
-    path('login/', views.login, name='login'),
+    path('', views.register, name='register'),
 
+    path('login', views.login_user, name='login'),
 ]
 
 
--- healthapp/views.py
+++ healthapp/views.py
@@ -7,6 +7,13 @@ from healthapp.models import *
 from requests.auth import HTTPBasicAuth
 from healthapp.credentials import MpesaAccessToken, LipanaMpesaPpassword
 
+#registration import
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+#login import
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
 def home (request):
@@ -194,7 +201,49 @@ def transactions_list(request):
     return render(request, 'transactions.html', {'transactions': transactions})
 
 def register (request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+
+        # Check the password
+        if password == confirm_password:
+            try:
+                user = User.objects.create_user(username=username, password=password)
+                user.save()
+
+                # Display a message
+                messages.success(request, "Account created successfully")
+                return redirect('/login')
+            except:
+                # Display a message if the above fails
+                messages.error(request, "Username already exist")
+        else:
+            # Display a message saying passwords don't match
+            messages.error(request, "Passwords do not match")
+
     return render(request, 'register.html')
 
-def login (request):
+
+def login_user (request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        # Check if the user exists
+        if user is not None:
+            # login(request, user)
+            login(request, user)
+            messages.success(request, "You are now logged in!")
+            # Admin
+            if user.is_superuser:
+                return redirect('/show')
+
+            # For Normal Users
+            return redirect('/home')
+        else:
+            messages.error(request, "Invalid login credentials")
+
     return render(request, 'login.html')
